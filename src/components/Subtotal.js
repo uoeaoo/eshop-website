7a4fe76d4b4a0e922b2db5212c07bdd88bdea647
uoeaoo <uoeaoo@gmail.com>
2023-05-30 11:01:06 +0300

    version 2

--- src/components/Subtotal.js
+++ src/components/Subtotal.js
@@ -1,12 +1,7 @@
 import React from 'react'
 import "./Subtotal.css"
 import { NumberFormatBase } from 'react-number-format'
-/* if there are problem with react-currency-format, you should write down a next comand in your terminal: npm install react-currency-format --save --force
-currently the react-currency-format does not work with the 18.0.0 and above to get the react-currency format to work you will need to downgrade your react to react ^17.0.0 in your package.json file after deleting your package-lock.json
 
-another option is to add --force to the end of the install command like so
-
-npm install react-currency-format --save --force*/
 import { useStateValue } from './StateProvider'
 import { getBasketTotal } from '../reducer'
 
